efe08ec37919cdc5bc26249102d442bc665ce8ee
antipiebse <s4223425@naver.com>
2022-07-29 05:21:47 +0900

    [feat] database연결 및 라우트 설정

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,17 +1,17 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { User } from "./entity/User"
+import { User } from './entities/User';
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import { Board } from './entities/Board';
+import { Comment } from './entities/Comment';
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "test",
-    password: "test",
-    database: "test",
-    synchronize: true,
-    logging: false,
-    entities: [User],
-    migrations: [],
-    subscribers: [],
-})
+  type: 'mysql',
+  host: process.env.DB_USERNAME,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DBNAME,
+  synchronize: true,
+  logging: false,
+  entities: [User, Board, Comment],
+});
--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,40 @@
-import * as express from "express"
-import * as bodyParser from "body-parser"
-import { Request, Response } from "express"
-import { AppDataSource } from "./data-source"
-import { Routes } from "./routes"
-import { User } from "./entity/User"
+import 'reflect-metadata';
+import * as express from 'express';
+import { AppDataSource } from './data-source';
+import { Routes } from './routes';
+import { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 
+// Connect typeORM mysql
 AppDataSource.initialize().then(async () => {
-
-    // create express app
-    const app = express()
-    app.use(bodyParser.json())
-
-    // register express routes from defined application routes
-    Routes.forEach(route => {
-        (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
-            const result = (new (route.controller as any))[route.action](req, res, next)
-            if (result instanceof Promise) {
-                result.then(result => result !== null && result !== undefined ? res.send(result) : undefined)
-
-            } else if (result !== null && result !== undefined) {
-                res.json(result)
-            }
-        })
-    })
-
-    // setup express app here
-    // ...
-
-    // start express server
-    app.listen(3000)
-
-    // insert new users for test
-    await AppDataSource.manager.save(
-        AppDataSource.manager.create(User, {
-            firstName: "Timber",
-            lastName: "Saw",
-            age: 27
-        })
-    )
-
-    await AppDataSource.manager.save(
-        AppDataSource.manager.create(User, {
-            firstName: "Phantom",
-            lastName: "Assassin",
-            age: 24
-        })
-    )
-
-    console.log("Express server has started on port 3000. Open http://localhost:3000/users to see results")
-
-}).catch(error => console.log(error))
+  // Create express server
+  const app = express();
+  // register express routes from defined application routes
+  Routes.forEach((route) => {
+    (app as any)[route.method](
+      route.route,
+      (req: Request, res: Response, next: Function) => {
+        const result = new (route.controller as any)()[route.action](
+          req,
+          res,
+          next,
+        );
+        if (result instanceof Promise) {
+          result.then((result) =>
+            result !== null && result !== undefined
+              ? res.send(result)
+              : undefined,
+          );
+        } else if (result !== null && result !== undefined) {
+          res.json(result);
+        }
+      },
+    );
+  });
+  app.use(express.json);
+  app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.error(err);
+    res.json({ message: err.message });
+  });
+  app.listen(app.get('port'), () => console.log('aa'));
+});
